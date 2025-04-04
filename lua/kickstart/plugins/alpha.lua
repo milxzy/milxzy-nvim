a1266caa662696c043dc593ab1cdf244cbde238f
miles <milescollins9@gmail.com>
2025-04-04 09:44:35 -0500

    got alpha working

--- lua/kickstart/plugins/alpha.lua
+++ lua/kickstart/plugins/alpha.lua
@@ -4,48 +4,51 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     local alpha = require 'alpha'
-    local startify = require 'alpha.themes.startify'
     local dashboard = require 'alpha.themes.dashboard'
 
-    -- Custom Header
-    startify.section.header.val = {
+    -- Header
+    dashboard.section.header.val = {
       [[███╗   ███╗██╗██╗     ██╗  ██╗███████╗██╗   ██╗]],
       [[████╗ ████║██║██║     ╚██╗██╔╝███████╗╚██╗ ██╔╝]],
       [[██╔████╔██║██║██║      ╚███╔╝ ╚══███╔╝ ╚████╔╝ ]],
       [[██║╚██╔╝██║██║██║      ██╔██╗   ███╔╝   ╚██╔╝  ]],
       [[██║ ╚═╝ ██║██║███████╗██╔╝ ██╗███████╗   ██║   ]],
       [[╚═╝     ╚═╝╚═╝╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝   ]],
-      [[                Welcome to MILXZY                ]],
+      [[                Welcome Home                ]],
     }
 
-    local function footer()
-      return "Don't Stop Until You are Proud..."
-    end
+    -- Buttons
+    dashboard.section.buttons.val = {
+      dashboard.button('f', '󰈞  Find file', ':Telescope find_files<CR>'),
+      dashboard.button('n', '  New file', ':ene <BAR> startinsert <CR>'),
+      dashboard.button('r', '󰄉  Recent files', ':Telescope oldfiles<CR>'),
+      dashboard.button('e', '  Open File Explorer', ':NvimTreeToggle<CR>'),
+      dashboard.button('g', '  Find word', ':Telescope live_grep<CR>'),
+      dashboard.button('c', '  Config', ':e $MYVIMRC<CR>'),
+      dashboard.button('s', '  Restore session', ":lua require('persistence').load()<CR>"),
+      dashboard.button('q', '󰿅  Quit', ':qa<CR>'),
+    }
 
-    dashboard.section.footer.val = footer()
-
+    -- Footer
+    dashboard.section.footer.val = "No cap and gown I ain't go to class"
     dashboard.section.footer.opts.hl = 'Type'
+
+    -- Highlighting (optional)
     dashboard.section.header.opts.hl = 'Include'
     dashboard.section.buttons.opts.hl = 'Keyword'
 
+    -- Prevent interference
     dashboard.opts.opts.noautocmd = true
+
+    -- Final setup
     alpha.setup(dashboard.opts)
 
-    -- Set up alpha
-    alpha.setup(startify.config)
-
-    -- Add autocmd to open alpha on startup when no file arguments
+    -- Autocmd to open dashboard on start if no files passed
     vim.api.nvim_create_autocmd('VimEnter', {
       callback = function()
-        -- Only start Alpha when Neovim is started without arguments
-        local should_skip = false
-        if vim.fn.argc() > 0 or vim.fn.line2byte '$' ~= -1 or vim.opt.insertmode:get() then
-          should_skip = true
-        end
-
-        if not should_skip then
+        if vim.fn.argc() == 0 then
           require('alpha').start(true)
-          vim.cmd [[au FileType alpha setlocal nofoldenable]]
+          vim.cmd 'setlocal nofoldenable'
         end
       end,
     })
